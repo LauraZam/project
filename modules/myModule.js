9b65a7c56dc2a2414fd4f598910ec6cb8328c6b6
laurazam <77395682+LauraZam@users.noreply.github.com>
2021-06-02 19:08:17 +0600

    Add files via upload

---
+++ modules/myModule.js
@@ -0,0 +1,17 @@
+// function User(newUsername, newFullname) {
+//     // return{
+//         this.username = newUsername,
+//         this.fullname = newFullname
+//     // }
+//     this.sayhello
+// }
+// // let u = new User('john228', 'jonh mitchell')
+
+// module.exports = User
+function Car(model, color, price) {
+    this.model = model
+    this.color = color
+    this.price = price
+        
+}
+module.exports = Car
